980d4c0c60da36b47d58373fdb5e31636b061af4
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-10-02 21:44:18 +0300

    update port

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
+
 
 // Listen on port 5000
-app.listen(5000, () => {
+app.listen(port, () => {
     connectDB(); // Connect to MongoDB
-    console.log('Server started at http://localhost:5000');
+    console.log(`Server started at http://localhost:${port}`);
 
 });
 
