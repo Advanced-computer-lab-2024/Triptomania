a15c5b635658880758d546c52f2699c0835650c2
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-10-03 00:33:50 +0300

    update admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,2 +1,15 @@
+import express from 'express';
+import activityController from '../controllers/shared/activityController.js';  // Make sure this path is correct
 
+const router = express.Router();
 
+// add a new activity category
+router.post('/activities/addCategory', activityController.addCategory);
+// get all activity categories
+router.get('/activities/getCategories', activityController.getCategories);
+// update an activity category
+router.put('/activities/editCategory/:id', activityController.editCategory);
+// delete an actvity category
+router.delete('/activities/deleteCategory/:id', activityController.deleteCategory);
+
+export default router;
