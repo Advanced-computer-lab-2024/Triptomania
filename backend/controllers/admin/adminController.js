77df923e8800fdcc940c7a2e1e27375150cd1e5f
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-10-04 21:05:38 +0300

    add admin controller

---
+++ backend/controllers/admin/adminController.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import adminModel from '../../models/admin.js'
+
+const addAdmin = async (req, res) => {
+    const { adminName, adminUsername, adminPassword } = req.body;
+
+    if (!adminName || !adminUsername || !adminPassword) {
+        return res.status(400).json({ message: "Please fill in all fields" });
+    }
+
+    try {
+        const admin = new adminModel({
+            AdminName: adminName,
+            AdminUsername: adminUsername,
+            AdminPassword: adminPassword
+        });
+
+        await admin.save();
+
+        res.status(201).json({ message: "Admin added successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
+
+export default {
+    addAdmin
+}
