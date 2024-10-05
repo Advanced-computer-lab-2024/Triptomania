9fc3f20055b58dc4fdd7131972d6cfbfa5d3bb2f
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-10-06 02:11:54 +0300

    update admin controller

--- backend/controllers/admin/adminController.js
+++ backend/controllers/admin/adminController.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
-import adminModel from '../../models/admin.js'
+import adminModel from '../../models/admin.js';
+import touristModel from '../../models/tourist.js';
+import tourismGovernerModel from '../../models/tourist.js';
+import sellerModel from '../../models/seller.js';
+import tourGuideModel from '../../models/tourGuide.js';
+import advertiserModel from '../../models/advertiser.js';
 
 const addAdmin = async (req, res) => {
     const { adminName, adminUsername, adminPassword } = req.body;
@@ -18,6 +23,37 @@ const addAdmin = async (req, res) => {
         await admin.save();
 
         res.status(201).json({ message: "Admin added successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
+
+const deleteAccount = async (req, res) => {
+    try {
+        const {id, type} = req.body;
+        switch (type) {
+            case "tourist":
+                tourGuideModel.findByIdAndDelete(id);
+                break;
+            case "tourGuide":
+                tourGuideModel.findByIdAndDelete(id);
+                break;
+            case "seller":
+                sellerModel.findByIdAndDelete(id);
+                break;
+            case "advertiser":
+                advertiserModel.findByIdAndDelete(id);
+                break;
+            case "tourismGoverner":
+                tourismGovernerModel.findByIdAndDelete(id);
+                break;
+            case "admin":
+                adminModel.findByIdAndDelete(id);
+                break;
+            default:
+                res.status(400).json({ message: "Invalid type" });
+                break;
+        }
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" });
     }
