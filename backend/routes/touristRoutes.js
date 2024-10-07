3c46c058d2c3297e305a184e136d375e2078f68d
Youssef Mostafa <youseffmostafaa@gmail.com>
2024-10-08 00:04:40 +0300

    sort we filter ui bas mesh sha8al

--- backend/routes/touristRoutes.js
+++ backend/routes/touristRoutes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import  search  from '../controllers/tourist/searchController.js';
 import  viewUpcoming  from '../controllers/shared/viewController.js'; // Adjust path as necessary
 import filterByTag from '../controllers/shared/filterController.js';
-
+import filterItineraries from '../controllers/shared/activityController.js';
 const router = express.Router();
 
 // Search route
 router.post('/search', search.search);
 router.get('/upcoming', viewUpcoming.viewUpcoming);
 router.get('/filterByTag', filterByTag.filterByTag);
+router.get('/filterItineraries',filterItineraries.filterItineraries);
+router.get('/sort',filterItineraries.sortItineraries);
 
 export default router;
