69d338dccfed75adc2da481bdb60acf13f2d99bc
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-11-12 05:15:29 +0200

    fixes

--- backend/controllers/tourist/touristController.js
+++ backend/controllers/tourist/touristController.js
@@ -332,7 +332,7 @@ export const rateTourGuide = async (req, res) => {
 
     // Check if the tourist completed the itinerary by checking bookingMade
     if (!itinerary.bookingMade.includes(touristId)) {
-      return res.status(403).json({ error: 'You have not booked this itinerary' });
+      return res.status(403).json({ error: 'You have not booked an itinerary with this tour guide' });
     }
 
     // Check if the itinerary has ended by comparing the end date with the current date
