e5d6d03715e72948c4cc286537d332e1ffb80a42
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-11-28 01:34:11 +0200

    use cookies

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,36 +6,39 @@ import tokenBlacklistModel from "../models/tokenBlacklist.js";
 dotenv.config();
 
 const verifyToken = async (req, res, next, allowedRoles) => {
-  // Check if the Authorization header exists
-  const token = req.headers['authorization'] && req.headers['authorization'].startsWith('Bearer ')
-    ? req.headers['authorization'].split(' ')[1]
-    : null;
+  // Retrieve the token from cookies
+  const token = req.cookies?.token;
 
   if (!token) {
     return res.status(403).json({ message: 'Access denied. No token provided.' });
   }
 
-  const blacklistedToken = await tokenBlacklistModel.findOne({ token: token });
-  if (blacklistedToken) {
-    return res.status(401).send("Invalid or expired token");
+  try {
+    // Check if the token is blacklisted
+    const blacklistedToken = await tokenBlacklistModel.findOne({ token: token });
+    if (blacklistedToken) {
+      return res.status(401).send("Invalid or expired token");
+    }
+
+    // Verify the token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid or expired token.' });
+      }
+
+      // Check if the user has the required role
+      if (allowedRoles && !allowedRoles.includes(decoded.type)) {
+        return res.status(403).json({ message: 'Forbidden: Insufficient role' });
+      }
+
+      // Store decoded user info in the request object
+      req.user = decoded;
+
+      next();
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error.' });
   }
-
-  // Verify token
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Invalid or expired token.' });
-    }
-
-    // Check if the user has the required role
-    if (allowedRoles && !allowedRoles.includes(decoded.type)) {
-      return res.status(403).json({ message: 'Forbidden: Insufficient role' });
-    }
-
-    // Store decoded user info in request object
-    req.user = decoded;
-
-    next();
-  });
 };
 
 const checkBlacklistedToken = async (req, res, next) => {
