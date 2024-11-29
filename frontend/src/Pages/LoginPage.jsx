5356dc16f545258deffea37e66b485cf35eebe9a
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-11-30 01:54:21 +0200

    Fix style blees

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../index.css';
 import styles from './login.module.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -27,60 +28,62 @@ const LoginPage = () => {
   };
 
   return (
-    <div className={styles['login-container']}>
-      <div className={styles['login-box']}>
-        <h1>Triptomania</h1>
-        <form onSubmit={handleLogin}>
-          <div className={styles['form-group']}>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              placeholder="Enter your username"
-            />
-          </div>
-          <div className={styles['form-group']}>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              placeholder="Enter your password"
-            />
-          </div>
-          <div className={`${styles['form-group']} ${styles['flex-container']}`}>
-            <select
-              value={type}
-              onChange={(e) => setType(e.target.value)}
-              required
-              className={styles['dropdown-menu']}
-            >
-              <option value="" disabled>Select user type</option>
-              <option value="admin">Admin</option>
-              <option value="advertiser">Advertiser</option>
-              <option value="seller">Seller</option>
-              <option value="tour-guide">Tour Guide</option>
-              <option value="tourism-governor">Tourism Governor</option>
-              <option value="tourist">Tourist</option>
-            </select>
+    <div className='login-page-wrapper'>
+      <div className={styles['login-container']}>
+        <div className={styles['login-box']}>
+          <h1>Triptomania</h1>
+          <form onSubmit={handleLogin}>
+            <div className={styles['form-group']}>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className={styles['form-group']}>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className={`${styles['form-group']} ${styles['flex-container']}`}>
+              <select
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+                required
+                className={styles['dropdown-menu']}
+              >
+                <option value="" disabled>Select user type</option>
+                <option value="admin">Admin</option>
+                <option value="advertiser">Advertiser</option>
+                <option value="seller">Seller</option>
+                <option value="tour-guide">Tour Guide</option>
+                <option value="tourism-governor">Tourism Governor</option>
+                <option value="tourist">Tourist</option>
+              </select>
+              <p
+                className={styles['forgot-password']}
+                onClick={() => navigate('/reset-password')}
+              >
+                Forgot Password?
+              </p>
+            </div>
+            {error && <p className={styles['error-message']}>{error}</p>}
+            <button type="submit" className={styles['login-button']}>Login</button>
             <p
-              className={styles['forgot-password']}
-              onClick={() => navigate('/reset-password')}
+              className={styles['signup-button']}
+              // Uncomment below if you want to enable navigation
+              // onClick={() => navigate('/sign-up')}
             >
-              Forgot Password?
+              Don't have an account? Sign up
             </p>
-          </div>
-          {error && <p className={styles['error-message']}>{error}</p>}
-          <button type="submit" className={styles['login-button']}>Login</button>
-          <p
-            className={styles['signup-button']}
-            // Uncomment below if you want to enable navigation
-            // onClick={() => navigate('/sign-up')}
-          >
-            Don't have an account? Sign up
-          </p>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
