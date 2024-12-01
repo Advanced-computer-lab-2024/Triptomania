8c35e1cf3f9008e36aa362eaa641838e71a17aa6
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-12-01 11:02:32 +0200

    add new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import LandingPage from './Pages/LandingPage';
 import ViewActivities from './Tourist/Activities/ViewActivities';
 import AddActivity from './Advertiser/Activities/AddActivity';
 import ViewProducts from './Admin/Products/ViewProducts';
+import RequestOtpPage from './Pages/auth/RequestOtp';
+import VerifyOtpPage from './Pages/auth/VerifyOtp';
+import NewPasswordPage from './Pages/auth/NewPassword';
 const App = () => {
   return (
     <Routes>
@@ -13,8 +16,11 @@ const App = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/activities" element={<ViewActivities />} />
-      <Route path="/advertiser/activities" element={<AddActivity />} />
+      <Route path="/advertiser/addActivity" element={<AddActivity />} />
       <Route path="/admin/products/viewproducts" element = {<ViewProducts/>} />
+      <Route path="/auth/requestOtp" element = {<RequestOtpPage/>} />
+      <Route path="/auth/verifyOtp" element = {<VerifyOtpPage/>} />
+      <Route path="/auth/newPassword" element = {<NewPasswordPage/>} />
       {/* <Route path="/SignUp" element={<SignUp />} /> */}
     </Routes>
   );
