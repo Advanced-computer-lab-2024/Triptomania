b80d7286061971e2046b0e983a8941680a867116
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-12-01 09:30:51 +0200

    update unnecessary token retrieval

--- backend/controllers/services/authController.js
+++ backend/controllers/services/authController.js
@@ -136,9 +136,8 @@ const logout = async (req, res) => {
     }
 };
 
-const forgotPassword = async (req, res) => {
-    const type = req.user.type;
-    const id = req.user._id;
+const requestOtp = async (req, res) => {
+    const { username, type } = req.body;
 
     // Validate type
     const userModel = userCollections[type];
@@ -146,7 +145,7 @@ const forgotPassword = async (req, res) => {
 
     try {
         // Check if the user exists
-        const user = await userModel.findOne({ _id: id });
+        const user = await userModel.findOne({ username: username });
         if (!user) return res.status(404).send('User not found');
 
         // Generate OTP
@@ -183,15 +182,13 @@ const forgotPassword = async (req, res) => {
 }
 
 const verifyOTP = async (req, res) => {
-    const type = req.user.type;
-    const id = req.user._id;
-    const { otp } = req.body;
+    const { otp, type, username } = req.body;
     const userModel = userCollections[type];
     if (!userModel) return res.status(400).send('Invalid user type');
 
     try {
         // Check if the user exists
-        const user = await userModel.findOne({ _id: id });
+        const user = await userModel.findOne({ username: username });
         if (!user) return res.status(404).send('User not found');
 
         // Check if the OTP is correct
@@ -273,4 +270,41 @@ const changePassword = async (req, res) => {
     }
 }
 
-export default { login, refreshToken, logout, forgotPassword, verifyOTP, changePassword };
+const changeForgotPassword = async (req, res) => {
+    try {
+        const { newPassword, type, username } = req.body;
+
+        // Check if newPassword meets minimum length
+        if (newPassword.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 characters long" });
+        }
+
+        const userModel = userCollections[type];
+
+        // Retrieve the account based on ID and type
+        let account = await userModel.findOne({ username: username });
+
+        if (!account) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+
+        // Verify the old password
+        const isMatch = await bcrypt.compare(newPassword, account.password);
+        if (isMatch) {
+            return res.status(400).json({ message: "New password can not be the same as the old password" });
+        }
+
+        // Hash the new password
+        const hashedPassword = await hashPassword(newPassword);
+
+        acount.password = hashedPassword;
+        await account.save();
+
+        res.status(200).json({ message: "Password changed successfully" });
+    } catch (error) {
+        console.error("Error changing password:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
+
+export default { login, refreshToken, logout, requestOtp, verifyOTP, changePassword, changeForgotPassword };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,8 +7,9 @@ const router = express.Router();
 router.post('/login', authController.login);
 router.post('/refresh-token', authMiddleware.checkBlacklistedToken, authController.refreshToken);
 router.post('/logout', authController.logout);
-router.post('/forgot-password', (req, res, next) => authMiddleware.verifyToken(req, res, next, ['admin', 'advertiser', 'seller', 'tourGuide', 'tourismGovernor', 'tourist']), authController.forgotPassword);
-router.post('/verifyOTP', (req, res, next) => authMiddleware.verifyToken(req, res, next, ['admin', 'advertiser', 'seller', 'tourGuide', 'tourismGovernor', 'tourist']), authController.verifyOTP);
+router.post('/requestOtp', authController.requestOtp);
+router.post('/verifyOTP', authController.verifyOTP);
 router.put('/changePassword', (req, res, next) => authMiddleware.verifyToken(req, res, next, ['admin', 'advertiser', 'seller', 'tourGuide', 'tourismGovernor', 'tourist']), authController.changePassword);
+router.put('/changeForgotPassword', authController.changeForgotPassword);
 
 export default router;
