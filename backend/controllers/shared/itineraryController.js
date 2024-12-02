ab280b3036c3eb671edba7da6dc386d413fd32f3
ralool <rolaibrahim132@gmail.com>
2024-12-02 16:27:40 +0200

    idk

--- backend/controllers/shared/itineraryController.js
+++ backend/controllers/shared/itineraryController.js
@@ -77,61 +77,7 @@ const addItinerary = async (req, res) => {
   try {
     let { Name, activities, locationsToVisit, timeLine, duration, language, price, availableDates, availableTimes, accesibility, pickUp, dropOff,  Start_date, End_date, Tags, creatorId, preferenceTags } = req.body;
 
-    // // Check that parameters are not empty
-    // if (!Name || !activities || !locationsToVisit || !timeLine || !duration || !language || !price || !availableDates || !availableTimes || !pickUp || !dropOff || !bookingMade || !creatorId) {
-    //   return res.status(400).json({ message: "All required fields must be provided." });
-    // }
-
-    // Check that parameters' type is right
-    // if(typeof Name !== 'string')
-    //     {
-    //         console.log(Name);
-    //         return res.status(400).json({ message: "Name must be a string"});
-    //     }
-    // if (Array.isArray(locationsToVisit) && locationsToVisit.every(item => typeof item === 'string'))
-    // {
-    //     return res.status(400).json({ message: "Locations must be an array of strings"});
-    // }
-    // if(typeof timeLine !== 'string')
-    // {
-    //     return res.status(400).json({ message: "Time line must be a string"});
-    // }
-    // if(typeof duration !== 'string')
-    // {
-    //     return res.status(400).json({ message: "Duration line must be a string"});
-    // }
-    // if(typeof language !== 'string')
-    // {
-    //     return res.status(400).json({ message: "Lnaguage line must be a string"});
-    // }
-    // if(typeof price !== 'Number')
-    // {
-    //     return res.status(400).json({ message: "Price line must be a Number"});
-    // }
-    // if (Array.isArray(activities) && activities.every(item => typeof item === 'string'))
-    //     {
-    //         return res.status(400).json({ message: "Activities must be a string"});
-    //     }
-    // if (Array.isArray(availableDates) && availableDates.every(item => typeof item === 'string'))
-    // {
-    //     return res.status(400).json({ message: "Available dates must be a string"});
-    // }
-    // if (Array.isArray(availableTimes) && availableTimes.every(item => typeof item === 'string'))
-    // {
-    //     return res.status(400).json({ message: "Available times must be a string"});
-    // }
-    // if (Array.isArray(accesibility) && accesibility.every(item => typeof item === 'string'))
-    // {
-    //     return res.status(400).json({ message: "Accesibility times must be a string"});
-    // }
-    // if(typeof pickUp !== 'string')
-    // {
-    //     return res.status(400).json({ message: "Pick up must be a string"});
-    // }
-    // if(typeof dropOff !== 'string')
-    // {
-    //     return res.status(400).json({ message: "Drop off must be a string"});
-    // }
+   
     if (!preferenceTags) {
       preferenceTags = [];
     }
