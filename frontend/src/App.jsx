962c8658a116a5cf2009a6ac09446b79aa3c6085
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-12-07 15:37:10 +0200

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,6 @@ import ViewMyItinerariesTourGuide from './TourGuide/Itineraries/ViewIMytinerarie
 import EditMyItinerary from './TourGuide/Itineraries/EditMyItinerary';
 import ProductDetails from './Tourist/Products/ProductDetails';
 import TouristCart from './Tourist/TouristCart/TouristCart';
-import EditMyItinerary from './TourGuide/Itineraries/editMyItinerary';
 
 const App = () => {
   return (
