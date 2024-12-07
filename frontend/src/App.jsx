14a90a2c91565f633a8098014eb87af7a5d6aab5
Omar Abdelwahab <maroabdelwahab@gmail.com>
2024-12-07 15:37:49 +0200

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ import AdvertiserHomePage from './Advertiser/AdvertiserHomePage';
 import ViewActivitiesAdvertiser from './Advertiser/Activities/ViewMyActivities';
 import TourGuideHomePage from './TourGuide/TourGuideHomePage';
 import ViewMyItinerariesTourGuide from './TourGuide/Itineraries/ViewIMytineraries';
-import EditMyItinerary from './TourGuide/Itineraries/EditMyItinerary';
+import EditMyItinerary from './TourGuide/Itineraries/editMyItinerary';
 import ProductDetails from './Tourist/Products/ProductDetails';
 import TouristCart from './Tourist/TouristCart/TouristCart';
 
